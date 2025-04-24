e4cd44b54b8aa18201f64356857f7ae8029f54f1
jo.zhou <jo.zhou@nio.com>
2025-04-24 09:47:35 +0800

    ver 0.71b 1. update shuffle data, start scaling law test
    todo:
    1. test cases for data loader
    2. add raw data in metrics.py
    3. additional dataset
    4. add PPO

--- utils/common.py
+++ utils/common.py
@@ -1,5 +1,7 @@
 import json
+import time
 from utils.decorator_train import finish, init
+from pytorch_lightning.callbacks import Callback
 from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint, ModelSummary, TQDMProgressBar
 
 
@@ -20,6 +22,40 @@ def decorator_function(train_function):
     return wrapper_function
 
 
+class ScalingLawCallback(Callback):
+    """
+    Logs a 'scaling_compute' metric at the end of each training epoch:
+      compute = num_params * tokens_processed * epoch_time
+    """
+
+    def __init__(self, seq_len: int):
+        super().__init__()
+        self.seq_len = seq_len
+        self.tokens_processed = 0
+        self.epoch_start_time = 0
+
+    def on_train_epoch_start(self, trainer, pl_module):
+        # reset counters at the start of each epoch
+        self.epoch_start_time = time.time()
+        self.tokens_processed = 0
+
+    def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
+        # assume batch is a dict with 'input_tokens' of shape [B, T] or similar
+        # count tokens = batch_size * sequence_length
+        self.tokens_processed += batch['input_ids'].shape[0] * batch['input_ids'].shape[1]
+
+    def on_train_epoch_end(self, trainer, pl_module):
+        epoch_time = time.time() - self.epoch_start_time
+        # total number of model parameters
+        num_params = sum(p.numel() for p in pl_module.parameters() if p.requires_grad)
+        compute = num_params * self.tokens_processed * epoch_time
+        # log both to the LightningModule logger and to the progress bar
+        pl_module.log('scaling_compute', compute, prog_bar=True, on_epoch=True, sync_dist=True)
+        print({'scaling_compute': compute})
+        if trainer.logger:
+            trainer.logger.log_metrics({'scaling_compute': compute}, step=trainer.current_epoch)
+
+
 def setup_callbacks(cfg_obj, monitor, mode):
     ckpt_callback = ModelCheckpoint(
         dirpath=cfg_obj.checkpoint_dir,
@@ -33,5 +69,6 @@ def setup_callbacks(cfg_obj, monitor, mode):
     progress_bar = TQDMProgressBar()
     model_summary = ModelSummary(max_depth=3)
     lr_monitor = LearningRateMonitor(logging_interval='epoch')
+    scaling_law = ScalingLawCallback(cfg_obj.max_frame)
 
-    return [ckpt_callback, progress_bar, model_summary, lr_monitor]
+    return [ckpt_callback, progress_bar, model_summary, lr_monitor, scaling_law]
